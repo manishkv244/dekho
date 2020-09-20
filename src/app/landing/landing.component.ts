258e0430e6d55ae0d7c89d00ace583a05556fd75
manish <manish.vishwakarma@neocepts.co.in>
2020-09-21 01:14:58 +0530

    updated

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,17 +7,34 @@ import { SwiperOptions } from 'swiper';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-
+  config:SwiperOptions;
+  config3:SwiperOptions;
   constructor() { }
 
   ngOnInit(): void {
+    this.slidePerViewDefault();
+    this.slidePerView3();
   }
-  config: SwiperOptions = {
-    pagination: { el: '.swiper-pagination', clickable: true },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    spaceBetween: 30
-  };
+
+  slidePerViewDefault(){
+    this.config= {
+      pagination: { el: '.swiper-pagination', clickable: true },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      spaceBetween: 20
+    };
+  }
+  slidePerView3(){
+    this.config3= {
+      pagination: { el: '.swiper-pagination', clickable: true },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      slidesPerView: 2,
+      spaceBetween: 20
+    };
+  }
 }
