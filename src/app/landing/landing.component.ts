993701d88b67a624d2b4648bc990345dbf31f2f2
manish <manish.vishwakarma@neocepts.co.in>
2020-09-22 00:18:01 +0530

    images added

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { SwiperOptions } from 'swiper';
 
 @Component({
@@ -9,32 +9,58 @@ import { SwiperOptions } from 'swiper';
 export class LandingComponent implements OnInit {
   config:SwiperOptions;
   config3:SwiperOptions;
-  constructor() { }
+  screenOrientation:boolean=true
+  constructor() {
+    this.orientation();
+   }
 
   ngOnInit(): void {
     this.slidePerViewDefault();
     this.slidePerView3();
+
+    
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.orientation();
+  }
   slidePerViewDefault(){
     this.config= {
       pagination: { el: '.swiper-pagination', clickable: true },
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
       },
-      spaceBetween: 20
+      spaceBetween: 20,
+      loop: true,
     };
   }
   slidePerView3(){
     this.config3= {
       pagination: { el: '.swiper-pagination', clickable: true },
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
       },
       slidesPerView: 2,
-      spaceBetween: 20
+      spaceBetween: 20,
     };
+  }
+
+  orientation(){
+    if (window.matchMedia("(orientation: landscape)").matches) {
+    this.screenOrientation=false
+   }else{
+    this.screenOrientation=true
+   }
   }
 }
